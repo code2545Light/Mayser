cd1223432ec525da4253774ca681f93d9cfd7d04
code2545Light <code2545Light@yahoo.co.jp>
2019-10-09 12:29:18 +0900

    一旦Ver1

--- MayBoot/MayBoot.asm
+++ MayBoot/MayBoot.asm
@@ -1,3 +1,4 @@
+;HELLO. SKKS
 ; FAT12 format
 
 ; - [Tips　IA32（x86）命令一覧](http://softwaretechnique.jp/OS_Development/Tips/IA32_instructions.html)
@@ -20,13 +21,14 @@
         ; http://elm-chan.org/docs/fat.html#notes
         ; BPB(BIOS Parameter Block)
                                 ; Name             | Offset              | Byte | Description
-                                ;                                               | 
+
         ; FAT12/16/32共通フィールド(オフセット0～35)
-        JMP     entry           ; BS_JmpBoot       | 0x0000-0x0002 0-2   |    3 | Jump to Bootstrap
-        DB      0x90            ;                                               | ブートストラッププログラムへのジャンプ命令(x86命令)。
-                                ;                                               | 0xEB, 0x??, 0x90 (ショート ジャンプ+NOP)
-        DB      "HELLOIPL"      ; BS_OEMName       | 0x0003-0x000a 3-10  |    8 | これは単なる名前である
-        DW      512             ; BPB_BytsPerSec   | 0x000b-0x000c 11-12 |    2 | セクタあたりのバイト数.
+        jmp     short entry     ;ジャンプ命令
+        nop                     ;何の操作も実行されません。この命令は、命令コードとしてメモリに存在しますが、
+                                ;EIPレジスタを除いて、プロセッサの実行に何の影響も与えない1バイトの命令です。0x90
+
+        DB      "MAYSSKKS"      ; BS_OEMName       | 0x0003-0x000a 3-10  |    8 | OEM ラベル名(フォーマットを行った OS やバージョン)
+        DW      512             ; BPB_BytsPerSec   | 0x000b-0x000c 11-12 |    2 | 論理セクタサイズ(通常は 0200h (512バイト/セクタ)
                                 ;                                               | 512, 1024, 2048, 4096
                                 ;                                               | Bytes Per Cluster
         DB      1               ; BPB_SecPerClus   | 0x000d           13 |    1 | アロケーションユニット(割り当て単位)当たりのセクタ数
@@ -38,7 +40,7 @@
         DB      2               ; BPB_NumFATs      | 0x0010           16 |    1 | FATの個数 
                                 ;                                               | (このフィールドの値は常に2に設定すべきである)
         DW      224             ; BPB_RootEntCnt   | 0x0011-0x0012 17-18 |    2 | FAT12/16ボリュームではルートディレクトリに
-                                ;                                               | 含まれるディレクトリエントリの数を示す.
+                                ;                                               | 含まれるディレクトリエントリの数を示す.ルートディレクトリエントリの最大数(1ディレクトリの最大ファイル数)
                                 ;                                               | このフィールドにはディレクトリテーブルのサイズが
                                 ;                                               | 2セクタ境界にアライメントする値,つまり,
                                 ;                                               | BPB_RootEntCnt*32がBPB_BytsPerSecの偶数倍になる値
@@ -97,10 +99,10 @@
                                 ;                  |                     |      | 
         DB      0x00            ; BS_DrvNum        | 0x0024           36 |    1 |
         DB      0x00            ; BS_Reserved1     | 0x0025           37 |    1 |
-        DB      0x29            ; BS_BootSig       | 0x0026           38 |    1 |
-
+        DB      0x29            ; BS_BootSig       | 0x0026           38 |    1 |ブートシグネチャ 29h の場合はボリュームシリアル番号を持つ
+                                ;                                               | 続く3つのフィールドがボリュームシリアル番号
         DD      0xffffffff      ; BS_VolID         | 0x0027-0x002a 39-42 |    4 | ボリュームシリアル番号
-        DB      "HELLO-OS   "   ; BS_VolLab        | 0x002a-0x0036 43-54 |   11 | ディスクの名前(ルートディレクトリに記録される11バイトのボリュームラベルに一致する)
+        DB      "MAYSSKKS   "   ; BS_VolLab        | 0x002a-0x0036 43-54 |   11 | ディスクの名前(ルートディレクトリに記録される11バイトのボリュームラベルに一致する)
         DB      "FAT12   "      ; BS_FilSysType    | 0x0036-0x003d 54-61 |    8 | フォーマットの名前
         RESB    18              ;                  | 0x003e-0x004f 62-79 |    8 | Reserve Bytes : [3.2.2 RESB and Friends: Declaring Uninitialized Data](https://www.nasm.us/doc/nasmdoc3.html#section-3.2.2)
                                 ;                                               | 18バイト空けて 0x7c50 の直前まで埋める
@@ -109,38 +111,6 @@
 
 
         ;=======================================================================
-        ; START BS_BootCode                                | 64      448
-        ; (ブートストラッププログラム. システム依存フィールドで、未使用時はゼロで埋める。)
-        ; 0x7c50
-        ;
-        ; rb (register byte),  rw (register word), rd (register double-word) 等の表記は
-        ; https://www.intel.co.jp/content/dam/www/public/ijkk/jp/ja/documents/developer/IA32_Arh_Dev_Man_Vol2A_i.pdf
-        ; によっている.
-        ; >   | rb | rw | rd  |
-        ; > 0 | AL | AX | EAX |
-        ; > 1 | CL | CX | ECX |
-        ; > 2 | DL | DX | EDX |
-        ; > 3 | BL | BX | EBX |
-        ; > 4 | AL | SP | ESP |
-        ; > 5 | CL | BP | EBP |
-        ; > 6 | DL | SI | ESI |
-        ; > 7 | BL | DI | EDI |
-        ; > imm8 － 即値バイト値。記号 imm8 は -128 から +127 までの符号付き数値である
-        ; == 16 bit register ==
-        ; AX : acumulator
-        ; CX : Counter
-        ; DX : Data
-        ; BX : Base
-        ; SP : Stack Pointer
-        ; BP : Base  Pointer
-        ; SI : Source      Index
-        ; DI : Destination Index
-        ; ES : Extra Segment
-        ; CS : Code  Segment
-        ; SS : Stack Segmengt
-        ; DS : Data  Segmengt
-        ; FS : no-name
-        ; GS : no-name
 
 entry:
         MOV     AX, 0           ; AX (rw1) に0(imm8)代入
@@ -149,12 +119,14 @@ entry:
         MOV     DS, AX
         MOV     ES, AX
 
+       	call    register_dump  ; レジスターのダンプ
+
         MOV     SI, msg
 putloop:
         MOV     AL, BYTE [SI]   ; BYTE (accumulator low)
         ADD     SI, 1           ; increment stack index
         CMP     AL, 0           ; compare (<end msg>)
-        JE      fin             ; jump to fin if equal to 0
+        JZ      fin             ; jump to fin if equal to 0
 
                                 ; 一文字表示
         MOV     AH, 0x0e        ; AH = 0x0e
@@ -168,20 +140,113 @@ fin:
 
 msg:
         DB      0x0a, 0x0a
-        DB      "HELLO! SKKS"
+        DB      "HELLO! SKKS2"
         DB      0x0a
         DB      0               ; end msg
 
-        ;RESB    0x7dfe-($-$$)  ; これだとエラーが出た。。。
-                                ; セクタサイズ 512 Byte なので 510 Byte目までを埋めたいときは
-                                ; 0x1fe - ($-$$) としてやればいい
-                                ; > you can tell how far into the section you are by using ($-$$)
-                                ; > [3.5 Expressions - NASM - The Netwide Assembler](https://www.nasm.us/doc/nasmdoc3.html#section-3.5)
-        RESB    0x1fe-($-$$)    ; 現在の場所から 0x1fd (0x1fe の直前)
-                                ; まで(残りの未使用領域)を0で埋める
-                                ; (naskでは0で初期化するみたいだがnasm
-                                ; だと初期化しない) 
-                                ; 0x7dfe-0x7c00 = 32254−31744 = 510
+
+;============================デバッグ用コード
+
+
+putstring:                          ;putstring 文字列表示
+        pusha                       ;汎用レジスタ全てへpushする。
+.put:   cs lodsb                    ;ひとつのピリオドで始まるラベルはローカルラベル
+                                    ;ストリング命令 lodsb を使って siレジスタのから1バイト→al へ
+                                    ;※※  cs lodsb としているのは、DSレジスタの代わりにCSを使うという事？
+                                    ;   そんな構文がある？？
+        or      al,al               ;al をOR
+        jz      .pute               ;結果が0なら、処理を終える
+        xor     bx,bx               ;bxレジスタを0に  ※※この理由は？
+        mov     ah,0x0e             ;ahレジスタを 0x0eに
+        int     0x10                ; ah=0E INT10h で、文字出力の割り込み
+        jmp     .put                ;.putへ戻る
+.pute:  popa                        ;汎用レジスタ全てをpop
+        ret                         ;呼び出しもとへ
+
+
+; tohex
+;   ax = data to convert and display, cl = shift count
+
+tohex:
+        pusha
+.tol:   rol     ax,4
+        push    ax
+        and     al,0x0f
+        cmp     al,0x0a
+        jb      .to
+        add     al,0x07
+.to:    add     al,0x30
+        mov     ah,0x0e
+        int     0x10
+        pop     ax
+        dec     cl
+        jnz     .tol
+        mov     ax,0x0e20
+        int     0x10
+        popa
+        ret
+
+memory_dump:
+        pusha
+        mov     bl,0x10
+.mem:   mov     ah,byte [es:di]
+        inc     di
+        mov     cl,0x02
+        call    tohex
+        dec     bl
+        jnz     .mem
+        mov     si,crlf
+        call    putstring
+        popa
+        ret
+
+;%ifdef DEBUG
+;register_dump デバッグ用
+;出力イメージ
+;0000 0000 ・・・
+;AX BX CX DX SI DI CS DS ES SP  それぞれレジスタの値
+register_dump:
+        pusha
+        push    sp
+        push    es
+        push    ds
+        push    cs
+        push    di
+        push    si
+        push    dx
+        push    cx
+        push    bx
+        push    ax
+        mov     cx,0x0a04
+.reg:   pop     ax
+        call    tohex
+        dec     ch
+        jnz     .reg
+        mov     si,crlf
+        call    putstring
+        popa
+        ret
+;%endif
+
+crlf    db      0x0d,0x0a,0
+
+
+
+
+        times   0x01fe-($-$$) db 0      ;0x1fe - 先頭からのここまでのバイト数分 0 で埋める
+                                        ;times はNASM
+					;NASM のtimes ディレクティブ
+					;times ディレクティブは GASの%rep と同じようにアセンブラー・レベルで動作し、やはり後に式が続きます。
+					;times <expression> nop
+					;times ディレクティブは、後に続く式を、指定回数繰り返す
+
+					;times 3 mov eax, 2
+					;下記と等価
+
+					;mov eax, 2
+					;mov eax, 2
+					;mov eax, 2
+
 
         ;=======================================================================
         ; END BS_BootCode       ; Name             | Offset              | Byte | Description
